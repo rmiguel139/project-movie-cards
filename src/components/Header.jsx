8b3600d3cc5f23452b5ac7d345ca05a934fc052a
Miguel <migmenezes.mm2@gmail.com>
2021-07-22 15:29:04 -0300

    requisito 1 e 2 + lint

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>
+          Movie Cards Library
+        </h1>
+      </header>
+    );
+  }
+}
+
+export default header;
